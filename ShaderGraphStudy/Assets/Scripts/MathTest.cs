f9a740777cc1bf81099a8d454132279aa338c4bd
xiezhifeng <827443469@qq.com>
2022-04-03 17:26:00 +0800

    shader test

--- ShaderGraphStudy/Assets/Scripts/MathTest.cs
+++ ShaderGraphStudy/Assets/Scripts/MathTest.cs
@@ -14,7 +14,7 @@ public class MathTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        //Graphics.DrawMeshInstanced
     }
 
     // Update is called once per frame
